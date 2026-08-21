c07b5e147291c05bdb89b3a79bb9990902b873d1
jinaa7710-cell <jinaa7710@gmail.com>
2026-08-21 18:17:19 +0500

    Kanbili
    On going

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+const CACHE_NAME="table-order-v1";
+const FILES=["./","./index.html","./manifest.json","./service-worker.js","./icon.svg"];
+self.addEventListener("install",event=>{
+  event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(FILES)).then(()=>self.skipWaiting()));
+});
+self.addEventListener("activate",event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener("fetch",event=>{
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
+    const copy=response.clone();
+    caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));
+    return response;
+  }).catch(()=>caches.match("./index.html"))));
+});
